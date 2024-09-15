3d75470f07b3df3fd6ee00aaccbecd6595675bc5
Julia Pan <julia.vl.pan@gmail.com>
2024-09-14 23:27:08 -0500

    disabling autoplay on videos

--- src/pages/ProjectDetails/ProjectDetails.tsx
+++ src/pages/ProjectDetails/ProjectDetails.tsx
@@ -46,7 +46,6 @@ export default function ProjectDetails() {
                     {alt}
                   </span>
                   <video
-                    autoPlay
                     controls
                     muted
                     loop
